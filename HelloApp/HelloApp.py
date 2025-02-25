2c70b7d764c624e1e2ae90fcdc769d37ecf4953a
Artem <portovartem2006@gmail.com>
2025-02-25 11:32:23 +0300

    finish

--- HelloApp/HelloApp.py
+++ HelloApp/HelloApp.py
@@ -6,3 +6,23 @@ for i in range(10):
     summ += i
 
 print(summ)
+
+def check_set(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+
+    if set1 == set2:
+        return True
+    else:
+        return False
+
+list1 = list(input('¬ведите первый список\n'))
+list2 = list(input('¬ведите второй список\n'))
+
+result = check_set(list1, list2)
+
+if result == True:
+    print('ћножества элементов данных списков совпадают')
+else:
+    print('ћножества элементов данных списков не совпадают')
+
